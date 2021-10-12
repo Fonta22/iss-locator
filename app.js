d8cebbdf8cf72c750e48fe40db6a301d3d7d5680
Fonta22 <61181201+Fonta22@users.noreply.github.com>
2021-10-12 14:28:36 +0200

    Database successfully implemented

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const app = express();
+const Datastore = require('nedb');
 
-app.listen(3000, () => console.log('listening at 3000'));
+const app = express();
+const port = 3000;
+const filedb = 'database.db';
+
+app.listen(port, () => console.log(`listening at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore(filedb);
+database.loadDatabase();
+
 app.post('/api', (request, response) => {
     const data = request.body;
-    console.log(data);
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    
+    database.insert(data);
+    console.log(`Data saved to database: "${filedb}"`);
+
     response.json({
         status: 'success',
         latitude: data.lat,

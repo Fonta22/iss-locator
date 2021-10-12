a9c18584f09e5ad24b175b60b61c82fe096f1fa1
Fonta22 <61181201+Fonta22@users.noreply.github.com>
2021-10-12 14:02:27 +0200

    Ready to implement database

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
+
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (request, response) => {
+    const data = request.body;
+    console.log(data);
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
+    });
+});
